a81f9ced84304807ae7f96b14aa52b410595af07
cafort <caleb.fort@gmail.com>
2017-09-27 16:14:06 -0500

    Submitting third assignment. Reconfigured image gallery to use array of objects and added counter

--- portfolio/src/script/gallery.js
+++ portfolio/src/script/gallery.js
@@ -3,24 +3,56 @@ $(document).ready(function () {
     //====== GALLERY =======
 
     function gallery() {
-        // Create array of image urls
-        var images = [
-            "http://via.placeholder.com/600x400",
-            "http://via.placeholder.com/600x400/fff/ddd",
-            "http://via.placeholder.com/600x400/a00/fff",
-            "http://via.placeholder.com/600x400/fff/000"
+        // Create array of image objects
+        var images = [{
+                "img": "../assets/selfie.jpg",
+                "cutline": "An old-fashioned selfie in a Hobby Lobby"
+            },
+            {
+                "img": "../assets/tablecat.jpg",
+                "cutline": "Cat under a chrome table"
+            },
+            {
+                "img": "../assets/waterslide.jpg",
+                "cutline": "An old water slide in Oklahoma"
+            },
+            {
+                "img": "../assets/eaglebike.jpg",
+                "cutline": "An eagle bike in downtown Springdale"
+            },
+            {
+                "img": "../assets/treeworker.jpg",
+                "cutline": "Helping to clean up the Fort Smith National Cemetery"
+            },
+            {
+                "img": "../assets/lightbulb.jpg",
+                "cutline": "Showing the heat from an incandescent bulb using an infrared camera"
+            },
+            {
+                "img": "../assets/mall.jpg",
+                "cutline": "Walking at the mall in Rogers"
+            },
         ];
 
-        //Set initial image index to zero and set initial image
-        var i = 0;
+        //Set up gallery slide container and the image, cutline, and counter
+        var gallerySlide = document.getElementById("slide");
         var galleryImage = document.getElementById("galleryImage");
-        galleryImage.src = images[i];
+        var galleryCutline = document.getElementById("galleryCutline");
+        var galleryCounter = document.getElementById("galleryCounter");
 
-        //jquery to fade out image, change source to index position, and fade in image
-        function imgFader(){
-            $(galleryImage).fadeOut(500, function(){
-                galleryImage.src = images[i];
-                $(galleryImage).fadeIn(500);
+        //Set initial index to zero and set initial img, cutline and counter
+        var i = 0;
+        galleryImage.src = images[i].img;
+        galleryCutline.innerHTML = images[i].cutline;
+        galleryCounter.innerHTML = [i + 1] + "/" + images.length;
+
+        //jquery to fade out image, change src, cutline and counter to index position, and fade in image
+        function imgFader() {
+            $(gallerySlide).fadeOut(500, function () {
+                galleryImage.src = images[i].img;
+                galleryCutline.innerHTML = images[i].cutline;
+                $(gallerySlide).fadeIn(500);
+                galleryCounter.innerHTML = [i + 1] + "/" + images.length;
             });
         }
 
@@ -34,7 +66,6 @@ $(document).ready(function () {
             } else {
                 i = 0;
             };
-            
             imgFader();
         };
 
@@ -48,7 +79,7 @@ $(document).ready(function () {
             } else {
                 i = images.length - 1;
             };
-            
+
             imgFader();
         }
     };
